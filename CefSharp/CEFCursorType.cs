58bf2dee98f1adb675a65651d5ca45ee50e45341
JordanVanZhang <jordanzhang1998@gmail.com>
2017-07-19 22:58:04 -0700

    add test

--- CefSharp/CEFCursorType.cs
+++ CefSharp/CEFCursorType.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2010-2016 The CefSharp Authors. All rights reserved.
+// Copyright � 2010-2016 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
@@ -37,6 +37,7 @@ namespace CefSharp
         SouthPanning,
         SoutheastPanning,
         SouthwestPanning,
+	   TestPanning,
         WestPanning,
         Move,
         VerticalText,
